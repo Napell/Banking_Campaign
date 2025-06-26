0b7cecb55c19fcba1d7b2450b01408120936492e
Napell <51301267+Napell@users.noreply.github.com>
2025-06-26 09:22:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -33,11 +33,11 @@ pdays = st.number_input("pdays", min_value=1, max_value=999)
 previous = st.number_input("previous", min_value=1, max_value=7)
 poutcome = st.selectbox("poutcome", ['nonexistent','failure',
 'success'])
-emp_var_rate = st.number_input("emp.var.rate")
-cons_price_idx= st.number_input("cons.price.idx")
-cons_conf_idx = st.number_input("cons.conf.idx")
+emp_var_rate = st.number_input("emp_var_rate")
+cons_price_idx= st.number_input("cons_price_idx")
+cons_conf_idx = st.number_input("cons_conf_idx")
 euribor3m = st.number_input("euribor3m")
-nr_employed = st.number_input("nr.employed")
+nr_employed = st.number_input("nr_employed")
 
 features=['age', 'duration', 'campaign', 'pdays', 'previous', 'emp.var.rate', 'cons.price.idx', 'cons.conf.idx', 'euribor3m', 'nr.employed', 'job_blue-collar', 'job_entrepreneur', 'job_housemaid', 'job_management', 'job_retired', 'job_self-employed', 'job_services', 'job_student', 'job_technician', 'job_unemployed', 'job_unknown', 'marital_married', 'marital_single', 'marital_unknown', 'education_basic.6y', 'education_basic.9y', 'education_high.school', 'education_illiterate', 'education_professional.course', 'education_university.degree', 'education_unknown', 'default_unknown', 'default_yes', 'housing_unknown', 'housing_yes', 'loan_unknown', 'loan_yes', 'contact_telephone', 'month_aug', 'month_dec', 'month_jul', 'month_jun', 'month_mar', 'month_may', 'month_nov', 'month_oct', 'month_sep', 'day_of_week_mon', 'day_of_week_thu', 'day_of_week_tue', 'day_of_week_wed', 'poutcome_nonexistent', 'poutcome_success
               ]
